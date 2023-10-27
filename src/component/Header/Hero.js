5db5817ccfbacbb43af8533249130232de8776b9
mdsujan-mriddha <mdsujan134523@gmail.com>
2023-10-27 23:10:32 +0600

    change on home page

--- src/component/Header/Hero.js
+++ src/component/Header/Hero.js
@@ -3,26 +3,32 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import heroOne from "../images/hero 1.jpg";
-import heroTwo from "../images/hero 2.jpg";
-import heroThree from "../images/hero 3.jpg";
+
+import { Suspense } from 'react';
+import lazyLoadedHeroOne from "../images/hero-1.jpg";
+import lazyLoadedHeroTwo from "../images/hero-2.jpg";
+import lazyLoadedHeroThree from "../images/hero-3.jpg";
+// Lazy load the images
+// const lazyLoadedHeroOne = lazy(() => import("../images/hero-1.jpg"));
+// const lazyLoadedHeroTwo = lazy(() => import("../images/hero-2.jpg"));
+// const lazyLoadedHeroThree = lazy(() => import("../images/hero-3.jpg"));
 const Hero = () => {
 
     const heroItem = [
         {
             id: 1,
             title: "Your market place for fresh farm products",
-            image: heroOne,
+            image: lazyLoadedHeroOne,
         },
         {
             id: 2,
             title: "কৃষি পণ্য প্রক্রিয়াজাতকরণ এবং বিশ্ব রফতানি বাজার",
-            image: heroTwo,
+            image: lazyLoadedHeroTwo,
         },
         {
             id: 3,
             title: "Your market place for fresh farm products",
-            image: heroThree,
+            image: lazyLoadedHeroThree,
         },
     ]
 
@@ -36,28 +42,31 @@ const Hero = () => {
                 disableOnInteraction: false,
             }}
             loop={true}
-            modules={[Navigation, Pagination,Autoplay]}
+            modules={[Navigation, Pagination, Autoplay]}
         // className='container'
         >
             {
                 heroItem.map((item) => (
                     <SwiperSlide key={item?.id}>
-                        <div className="hero" style={{ backgroundImage: `url("${item?.image}")`, height: "70vh", backgroundPosition: "center center", backgroundRepeat: "no-repeat", backgroundOrigin: 'content-box', objectFit: 'cover', backgroundSize: 'cover' }}>
-                            <div className="hero-overlay bg-opacity-70" ></div>
-                            <div className="hero-content text-center text-neutral-content">
-                                <div className="">
-                                    <h1 className="text-5xl font-bold text-white mb-5"> {item?.title} </h1>
-                                    <p className="mb-5 text-lg font-bold text-gray-300">Welcome to the Agrifutures <br />
-                                        where you can purchase products from fresh farms all over Bangladesh
-                                    </p>
-                                    <button className="btn btn-primary">Get Started</button>
+                        <Suspense fallback={<div> Loading... </div>}>
+                            <div className="hero" style={{ backgroundImage: `url("${item?.image}")`, height: "70vh", backgroundPosition: "center center", backgroundRepeat: "no-repeat", backgroundOrigin: 'content-box', objectFit: 'cover', backgroundSize: 'cover' }}>
+
+                                <div className="hero-overlay bg-opacity-70" ></div>
+                                <div className="hero-content text-center text-neutral-content">
+                                    <div className="">
+                                        <h1 className="text-5xl font-bold text-white mb-5"> {item?.title} </h1>
+                                        <p className="mb-5 text-lg font-bold text-gray-300">Welcome to the Agrifutures <br />
+                                            where you can purchase products from fresh farms all over Bangladesh
+                                        </p>
+                                        <button className="btn btn-primary">Get Started</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </Suspense>
                     </SwiperSlide>
                 ))
             }
-            
+
         </Swiper>
     );
 };
--- src/component/Products/OrganicFood.js
+++ src/component/Products/OrganicFood.js
@@ -3,6 +3,13 @@ import productOne from "../images/Organic product/Product 1.jpg";
 import productTwo from "../images/Organic product/product 2.jpg";
 import productThree from "../images/Organic product/product 3.jpg";
 import productFour from "../images/Organic product/product 4.jpg";
+import { LocationOn, Star } from '@mui/icons-material';
+import { Rating } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+
+
+
 
 const OrganicFood = () => {
 
@@ -35,21 +42,39 @@ const OrganicFood = () => {
 
     return (
         <Fragment>
-            <div className='container pt-20 pb-10'>
-                <h1 className='text-4xl font-bold text-primary lg:text-center'>অর্গানিক ফুড </h1>
-                <div className='pl-12 lg:pl-0 pt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12'>
+            <div className='pt-20 pb-10 lg:px-12 md:px-10'>
+                <div className="flex justify-between items-center">
+                    <h1 className='text-2xl font-bold text-primary lg:text-center'>অর্গানিক ফুড </h1>
+                    <Link to="/products" className='text-xl text-orange-700 font-bold'> View All </Link>
+                </div>
+                <div className='pt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
                     {
-                        products?.map((item) => (
-                            <div
-                                key={item?.id}
-                                className="card card-compact w-96 md:w-72 lg:w-64 bg-base-100 shadow-2xl">
-                                <figure className='lg:h-48 flex justify-center items-center'><img src={item?.image} alt="অর্গানিক_ফুড " className='w-full' style={{ objectFit: "cover", margin: "0 auto", width: '100%' }} /></figure>
-                                <div className="card-body pt-5">
-                                    <h2 className="text-2xl font-bold"> {item?.name} </h2>
-                                    <p className='text-lg font-medium'> মূল্য: {item?.price} টাকা  </p>
-                                    <div className="card-actions justify-end">
-                                        <button className="btn btn-primary">Buy Now</button>
+                        products?.map((product) => (
+                            <div className="w-96 h-auto bg-gray-100 rounded-lg shadow-xl relative" key={product.id}>
+                                <img loading='lazy' src={product?.image} alt={product.name}
+                                    className='p-1 rounded-2xl w-full h-64' />
+                                <div className='mt-5 px-2'>
+                                    <div className='flex justify-between gap-5 items-center'>
+                                        <p className='text-xl font-bold'> {product?.name} </p>
+                                        <Rating
+                                            style={{ color: 'orange' }}
+                                            name='review'
+                                            readOnly
+                                            precision={0.5}
+                                            value={4}
+                                            emptyIcon={<Star style={{ opacity: 0.55 }} fontSize="inherit" />}
+                                        />
                                     </div>
+                                    <p className='flex  gap-10 pt-2 text-gray-400 font-bold text-xl'> <LocationOn /> Barguna,Barishal </p>
+                                    <div className='flex gap-3 items-center p-2 pb-5'>
+                                        <p className='text-lg font-extrabold text-primary'> {product?.price} /- টাকা </p>
+                                        <p className='bg-gray-400 p-2 rounded-xl px-5'> 5% </p>
+                                    </div>
+                                </div>
+                                <div className='pb-1 mt-10'>
+                                    <Link
+                                        to="/products"
+                                        className='btn btn-primary w-full absolute bottom-0 left-0 hidden  transition-opacity opacity-0 delay-300 pt-4 justify-center items-center'> View details </Link>
                                 </div>
                             </div>
                         ))
