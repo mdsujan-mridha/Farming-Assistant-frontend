8367b6a30772e834fc624630059fda48f53e211c
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-01-28 13:19:40 +0600

    work on admin panel

--- src/component/Admin/Dashboard.js
+++ src/component/Admin/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Sidebar from './Sidebar';
 
 const Dashboard = () => {
     return (
         <div>
-            
+            <Sidebar />
         </div>
     );
 };
